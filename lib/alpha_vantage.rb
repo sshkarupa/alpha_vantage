940da2b473dba65170bd415cbc585276d51e191d
Sergey Shkarupa <s.shkarupa@gmail.com>
2021-10-16 11:57:15 +0200

    init

---
+++ lib/alpha_vantage.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "alpha_vantage/version"
+
+module AlphaVantage
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/alpha_vantage_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe AlphaVantage do
+  it "has a version number" do
+    expect(AlphaVantage::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
