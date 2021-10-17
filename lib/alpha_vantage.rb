806d760d05e67bb343a6f732eaf02e7cb0505fe9
Sergey Shkarupa <s.shkarupa@gmail.com>
2021-10-17 17:25:26 +0200

    add config

--- lib/alpha_vantage.rb
+++ lib/alpha_vantage.rb
@@ -1,8 +1,39 @@
 # frozen_string_literal: true
 
+require "ruby-next"
+require "ruby-next/language/setup"
+RubyNext::Language.setup_gem_load_path(transpile: true)
+
 require_relative "alpha_vantage/version"
+require_relative "alpha_vantage/ext/deep_transform"
+require_relative "alpha_vantage/ext/string"
+require_relative "alpha_vantage/ext/hash"
+
+require_relative "alpha_vantage/client"
 
 module AlphaVantage
   class Error < StandardError; end
-  # Your code goes here...
+
+  class ApiKeyMissing < Error; end
+
+  class << self
+    def config
+      @config ||= Config.new
+    end
+
+    def configure
+      yield(config)
+    end
+  end
+
+  class Config
+    attr_writer :api_key
+    def api_key
+      return @api_key if instance_variable_defined?(:@api_key)
+
+      raise ApiKeyMissing, "API key is missing"
+    end
+
+    def base_url = "https://www.alphavantage.co/query"
+  end
 end
