76930a36092de660d97f80e6f465f3c81e80d627
Sergey Shkarupa <s.shkarupa@gmail.com>
2021-10-23 19:41:59 +0200

    add specs for config

--- spec/alpha_vantage_spec.rb
+++ spec/alpha_vantage_spec.rb
@@ -5,7 +5,31 @@ RSpec.describe AlphaVantage do
     expect(AlphaVantage::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".config" do
+    let!(:config) { described_class.config }
+
+    it "returns the config" do
+      expect(config).to be_an_instance_of(AlphaVantage::Config)
+    end
+
+    context "without an api key" do
+      it "raises an error" do
+        expect { config.api_key }.to raise_error(AlphaVantage::ApiKeyMissing) do |e|
+          expect(e.message).to eq("API key is missing")
+        end
+      end
+    end
+
+    context "with an api key" do
+      before do
+        AlphaVantage.configure do |config|
+          config.api_key = "_api_key_"
+        end
+      end
+
+      it "returns an api key" do
+        expect(config.api_key).to eq("_api_key_")
+      end
+    end
   end
 end
