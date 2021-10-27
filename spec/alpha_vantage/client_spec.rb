14c294227a9d8080650331bb0590349d5cd586a4
Sergey Shkarupa <s.shkarupa@gmail.com>
2021-10-27 21:58:07 +0200

    refac: check that client makes an api calls with given attrs

--- spec/alpha_vantage/client_spec.rb
+++ spec/alpha_vantage/client_spec.rb
@@ -5,44 +5,23 @@ require "spec_helper"
 RSpec.describe AlphaVantage::Client do
   subject(:client) { described_class }
 
-  let!(:raw_data) do
-    {
-      "1. symbol": "TSLA",
-      "2. name": "Equity",
-      "3. type": "Equity",
-      "4. region": "United States",
-      "5. marketOpen": "09:30",
-      "6. marketClose": "16:00",
-      "7. timezone": "UTC-04",
-      "8. currency": "USD",
-      "9. matchScore": "0.8889"
-    }
-  end
-
-  let!(:data) do
-    {
-      symbol: "TSLA",
-      name: "Equity",
-      type: "Equity",
-      region: "United States",
-      market_open: "09:30",
-      market_close: "16:00",
-      timezone: "UTC-04",
-      currency: "USD",
-      match_score: "0.8889"
-    }
-  end
+  let(:url) { "https://www.alphavantage.co/query?apikey=_api_key_&datatype=json&function=SYMBOL_SEARCH&keywords=tesla" }
+  let(:response) { Net::HTTPSuccess.new(1.0, "200", "OK") }
 
   before do
-    stub_request(:get, "https://www.alphavantage.co/query")
-      .with(query: {apikey: "_api_key_", function: "SYMBOL_SEARCH", keywords: "tesla", datatype: "json"})
-      .to_return(body: {"bestMatches" => [raw_data]}.to_json, headers: {"Content-Type" => "application/json"})
+    allow(response).to receive(:body).and_return({BestMatches: []}.to_json)
+    allow(Net::HTTP).to receive(:get_response).with(URI.parse(url)).and_return(response)
   end
 
   describe ".get" do
-    it "makes api call" do
+    it "makes an api call" do
+      client.get(function: "SYMBOL_SEARCH", keywords: "tesla")
+      expect(Net::HTTP).to have_received(:get_response).once
+    end
+
+    it "returns a struct object" do
       result = client.get(function: "SYMBOL_SEARCH", keywords: "tesla")
-      expect(result).to have_attributes(best_matches: [data])
+      expect(result).to have_attributes(best_matches: [])
     end
   end
 end
