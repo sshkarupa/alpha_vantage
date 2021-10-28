29ea43206fef1b82b10e935643b1a5d20e7639b6
Sergey Shkarupa <s.shkarupa@gmail.com>
2021-10-28 22:54:26 +0200

    use doc format only for one spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
+  config.default_formatter = "doc" if config.files_to_run.one?
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
 
