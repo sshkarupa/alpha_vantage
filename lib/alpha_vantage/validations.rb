3c1a4fc2f76271bc1269144976567b886b516fc9
Sergey Shkarupa <s.shkarupa@gmail.com>
2021-10-28 22:55:17 +0200

    fix: use constant insead of method

--- lib/alpha_vantage/validations.rb
+++ lib/alpha_vantage/validations.rb
@@ -2,12 +2,16 @@
 
 module AlphaVantage
   module Validations
+    VALID_SLICES = (1..2).map do |year|
+      (1..12).map { |month| "year#{year}month#{month}".to_sym }
+    end.flatten.freeze
+
     VALID_VALUES = {
       interval: %w[1min 5min 15min 30min 60min],
       datatype: %w[json csv],
       outputsize: %w[compact full],
       adjusted: %w[true false],
-      slice: slaces
+      slice: VALID_SLICES
     }.freeze
 
     private
@@ -20,13 +24,7 @@ module AlphaVantage
       values = VALID_VALUES[type]
       return if values.include?(value)
 
-      raise ArgumentError, "Invalid #{type}, only allowed: #{values.join(', ')}"
-    end
-
-    def slices
-      @slaces ||= (1..2).map do |year|
-        (1..12).map { |month| "year#{year}month#{month}".to_sym }
-      end.flatten
+      raise ArgumentError, "Invalid #{type}, only allowed: #{values.join(", ")}"
     end
   end
 end
