28c679dff95831bfc322e255e3e60334520ffbc5
Sergey Shkarupa <s.shkarupa@gmail.com>
2021-10-31 21:35:02 +0100

    add AlphaVantage::TimeSeries api

--- lib/alpha_vantage/time_series.rb
+++ lib/alpha_vantage/time_series.rb
@@ -7,7 +7,14 @@ module AlphaVantage
     FUNCTIONS = {
       search: "SYMBOL_SEARCH",
       intraday: "TIME_SERIES_INTRADAY",
-      intraday_extended: "TIME_SERIES_INTRADAY_EXTENDED"
+      intraday_extended: "TIME_SERIES_INTRADAY_EXTENDED",
+      daily: "TIME_SERIES_DAILY",
+      daily_adjusted: "TIME_SERIES_DAILY_ADJUSTED",
+      weekly: "TIME_SERIES_WEEKLY",
+      weekly_adjusted: "TIME_SERIES_WEEKLY_ADJUSTED",
+      monthly: "TIME_SERIES_MONTHLY",
+      monthly_adjusted: "TIME_SERIES_MONTHLY_ADJUSTED",
+      quote: "GLOBAL_QUOTE"
     }.freeze
 
     def self.search(keywords:)
@@ -20,31 +27,60 @@ module AlphaVantage
       @symbol = symbol
     end
 
+    def quote
+      Client.get(function: FUNCTIONS[:quote], symbol: symbol).global_quote
+    end
+
     def intraday(**opts)
       params = {
-        function: FUNCTIONS[:intraday],
-        symbol: symbol,
+        datatype: opts.fetch(:datatype, "json"),
         outputsize: opts.fetch(:outputsize, "compact"),
         interval: opts.fetch(:interval, "5min"),
-        datatype: opts.fetch(:datatype, "json"),
         adjusted: opts.fetch(:adjusted, true).to_s
       }.then(&method(:validate))
 
-      Client.get(**params)
+      Client.get(function: FUNCTIONS[:intraday], symbol: symbol, **params)
     end
 
     def intraday_extended(**opts)
       params = {
-        function: FUNCTIONS[:intraday_extended],
-        symbol: symbol,
+        datatype: "csv",
         outputsize: opts.fetch(:outputsize, "compact"),
         interval: opts.fetch(:interval, "5min"),
-        datatype: "csv",
         adjusted: opts.fetch(:adjusted, true).to_s,
         slice: opts.fetch(:slice, "year1month1")
       }.then(&method(:validate))
 
-      Client.get(**params)
+      Client.get(function: FUNCTIONS[:intraday_extended], symbol: symbol, **params)
+    end
+
+    def daily(adjusted: false, **opts)
+      params = {
+        datatype: opts.fetch(:datatype, "json"),
+        outputsize: opts.fetch(:outputsize, "compact"),
+        interval: opts.fetch(:interval, "5min")
+      }.then(&method(:validate))
+
+      function = adjusted ? FUNCTIONS[:daily_adjusted] : FUNCTIONS[:daily]
+      Client.get(function: function, symbol: symbol, **params)
+    end
+
+    def weekly(adjusted: false, **opts)
+      params = {
+        datatype: opts.fetch(:datatype, "json")
+      }.then(&method(:validate))
+
+      function = adjusted ? FUNCTIONS[:weekly_adjusted] : FUNCTIONS[:weekly]
+      Client.get(function: function, symbol: symbol, **params)
+    end
+
+    def monthly(adjusted: false, **opts)
+      params = {
+        datatype: opts.fetch(:datatype, "json")
+      }.then(&method(:validate))
+
+      function = adjusted ? FUNCTIONS[:monthly_adjusted] : FUNCTIONS[:monthly]
+      Client.get(function: function, symbol: symbol, **params)
     end
   end
 end
--- spec/alpha_vantage/time_series_spec.rb
+++ spec/alpha_vantage/time_series_spec.rb
@@ -22,6 +22,22 @@ RSpec.describe AlphaVantage::TimeSeries do
     end
   end
 
+  describe "#quote" do
+    let!(:body) { open_fixture_file("time_series/quote.json") }
+    let!(:result) { yaml_fixture_file("time_series/quote.yml") }
+    let(:query) { base_query.merge(function: "GLOBAL_QUOTE") }
+
+    before do
+      stub_request(:get, "https://www.alphavantage.co/query")
+        .with(query: query)
+        .to_return(body: body, headers: {"Content-Type" => "application/json"})
+    end
+
+    it "returns a global quote" do
+      expect(time_series.quote).to match_array(result)
+    end
+  end
+
   describe "#intraday" do
     let!(:body) { open_fixture_file("time_series/intraday.json") }
     let!(:result) { yaml_fixture_file("time_series/intraday.yml") }
@@ -61,8 +77,109 @@ RSpec.describe AlphaVantage::TimeSeries do
     end
   end
 
-  describe "#daily"
-  describe "#weekly"
-  describe "#monthly"
-  describe "quote"
+  describe "#daily" do
+    context "when adjusted: false" do
+      let!(:body) { open_fixture_file("time_series/daily.json") }
+      let!(:result) { yaml_fixture_file("time_series/daily.yml") }
+      let(:query) do
+        base_query.merge(function: "TIME_SERIES_DAILY", interval: "5min", outputsize: "compact")
+      end
+
+      before do
+        stub_request(:get, "https://www.alphavantage.co/query")
+          .with(query: query)
+          .to_return(body: body, headers: {"Content-Type" => "application/json"})
+      end
+
+      it "returns a daily data" do
+        expect(time_series.daily.time_series_daily).to match_array(result)
+      end
+    end
+
+    context "when adjusted: true" do
+      let!(:body) { open_fixture_file("time_series/daily_adjusted.json") }
+      let!(:result) { yaml_fixture_file("time_series/daily_adjusted.yml") }
+      let(:query) do
+        base_query.merge(function: "TIME_SERIES_DAILY_ADJUSTED", interval: "5min", outputsize: "compact")
+      end
+
+      before do
+        stub_request(:get, "https://www.alphavantage.co/query")
+          .with(query: query)
+          .to_return(body: body, headers: {"Content-Type" => "application/json"})
+      end
+
+      it "returns a daily data" do
+        expect(time_series.daily(adjusted: true).time_series_daily).to match_array(result)
+      end
+    end
+  end
+
+  describe "#weekly" do
+    context "when adjusted: false" do
+      let!(:body) { open_fixture_file("time_series/weekly.json") }
+      let!(:result) { yaml_fixture_file("time_series/weekly.yml") }
+      let(:query) { base_query.merge(function: "TIME_SERIES_WEEKLY") }
+
+      before do
+        stub_request(:get, "https://www.alphavantage.co/query")
+          .with(query: query)
+          .to_return(body: body, headers: {"Content-Type" => "application/json"})
+      end
+
+      it "returns a daily data" do
+        expect(time_series.weekly.weekly_time_series).to match_array(result)
+      end
+    end
+
+    context "when adjusted: true" do
+      let!(:body) { open_fixture_file("time_series/weekly_adjusted.json") }
+      let!(:result) { yaml_fixture_file("time_series/weekly_adjusted.yml") }
+      let(:query) { base_query.merge(function: "TIME_SERIES_WEEKLY_ADJUSTED") }
+
+      before do
+        stub_request(:get, "https://www.alphavantage.co/query")
+          .with(query: query)
+          .to_return(body: body, headers: {"Content-Type" => "application/json"})
+      end
+
+      it "returns a daily data" do
+        expect(time_series.weekly(adjusted: true).weekly_adjusted_time_series).to match_array(result)
+      end
+    end
+  end
+
+  describe "#monthly" do
+    context "when adjusted: false" do
+      let!(:body) { open_fixture_file("time_series/monthly.json") }
+      let!(:result) { yaml_fixture_file("time_series/monthly.yml") }
+      let(:query) { base_query.merge(function: "TIME_SERIES_MONTHLY") }
+
+      before do
+        stub_request(:get, "https://www.alphavantage.co/query")
+          .with(query: query)
+          .to_return(body: body, headers: {"Content-Type" => "application/json"})
+      end
+
+      it "returns a daily data" do
+        expect(time_series.monthly.monthly_time_series).to match_array(result)
+      end
+    end
+
+    context "when adjusted: true" do
+      let!(:body) { open_fixture_file("time_series/monthly_adjusted.json") }
+      let!(:result) { yaml_fixture_file("time_series/monthly_adjusted.yml") }
+      let(:query) { base_query.merge(function: "TIME_SERIES_MONTHLY_ADJUSTED") }
+
+      before do
+        stub_request(:get, "https://www.alphavantage.co/query")
+          .with(query: query)
+          .to_return(body: body, headers: {"Content-Type" => "application/json"})
+      end
+
+      it "returns a daily data" do
+        expect(time_series.monthly(adjusted: true).monthly_adjusted_time_series).to match_array(result)
+      end
+    end
+  end
 end
